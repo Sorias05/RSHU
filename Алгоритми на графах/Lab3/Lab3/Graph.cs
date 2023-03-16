f954f88e0592a6b30e1c450f2ef57df2c6b1f3bf
Sorias05 <sashko2b3@gmail.com>
2023-03-16 08:54:39 +0200

    Алгоритми: Lab 3

---
+++ Алгоритми на графах/Lab3/Lab3/Graph.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    internal class Graph
+    {
+        //Списки для зберігання вершин та ребер
+        List<Vertex> Vertexes = new List<Vertex>();
+        List<Edge> Edges = new List<Edge>();
+        //повертаємо кількість вершин графа
+        public int VertexCount => Vertexes.Count;
+        //повертаємо кількість ребер графа
+        public int EdgeCount => Edges.Count;
+        //додаємо вершину
+        public void AddVertex(Vertex vertex)
+        {
+            Vertexes.Add(vertex);
+        }
+        //додаємо ребро
+        public void AddEdge(Vertex from, Vertex to)
+        {
+            var edge = new Edge(from, to);
+            Edges.Add(edge);
+        }
+        //Матриця суміжності
+        public int[,] Adjacency_Matrix()
+        {
+            var matrix = new int[Vertexes.Count, Vertexes.Count];
+            foreach (var edge in Edges)
+            {
+                var row = edge.From.Number;
+                var column = edge.To.Number;
+                matrix[row - 1, column - 1] = edge.Weight;// -1 бо в масиві елементи починаються із 0 а вершини графу з 1
+            }
+            return matrix;
+        }
+        //матриця інцидентності
+        public int[,] Incidence_Matrix()
+        {
+            var matrix = new int[Vertexes.Count, Edges.Count];
+            for (int i = 0; i < Edges.Count; i++)
+            {
+                matrix[Edges[i].From.Number - 1, i] = 1;// -1 бо в масиві елементи починаються із 0 а вершини графу з 1
+                matrix[Edges[i].To.Number - 1, i] = 1;
+            }
+            return matrix;
+        }
+        //повертаємо вершину за його індексом
+        public Vertex Vertex(int i)
+        {
+            return Vertexes[i];
+        }
+        //повертаємо ребро за його індексом
+        public Edge Edge(int i)
+        {
+            return Edges[i];
+        }
+        public List<int>[] adjacency_list()
+        {
+            List<int>[] list = new List<int>[VertexCount]; //сюди помістимо результат
+            for (int i = 0; i < VertexCount; i++) list[i] = new List<int>();//створюємо всі елементи списку
+            foreach (Edge edge in Edges) list[edge.From.Number - 1].Add(edge.To.Number - 1);//додаємо в список зв'язок між вершинами
+                                                                                            // -1 бо у нас вершини починаються з 1-ці а елементи списку з 0-ля
+            return list;
+        }
+        //Пошук шляху від однієї вершини до іншої за допомогою алгоритму пошуку в ширину (BFS) Breadth First Search
+        public List<int> BFS(int start, int dest)
+        {
+            start = start - 1; dest = dest - 1;//переходимо до нумерації вершин з 0, для правильної роботи алгоритму
+            List<int>[] adj = adjacency_list();//формуємо список суміжності вершин графу
+            bool[] visited = new bool[VertexCount]; //створюємо масив відвіданих вершин
+            int[] parent = new int[VertexCount]; //створюємо масив вершин "батьків"
+            for (int i = 0; i < VertexCount; i++) //задаємо масивам початкові значення
+            {
+                visited[i] = false;//жодну вершину ми ще не проходили
+                parent[i] = -1;//в жодної вершини немає "батька"
+            }
+            Queue<int> q = new Queue<int>();//створюємо чергу, в яку будемо поміщати вершини які потрібно обійти
+            visited[start] = true;//помічаємо початкову вершину відвіданою
+            q.Enqueue(start);//додаємо її (початкову вершину) в чергу
+            while (q.Count != 0) //поки ще лишилися елементи в черзі
+            {
+                int u = q.Dequeue();//виймаємо слідуючу вершину з черги і поміщаємо її у u
+                foreach (int v in adj[u])//перебираємо всі вершини із списку суміжності графа через змінну v
+                {
+                    if (!visited[v]) //якщо в цій вершины ми ще не були
+                    {
+                        visited[v] = true; //помычаэмо її як відвідану
+                        parent[v] = u;//додаємо батька (у вершину v ми можемо потрапити з вершини u)
+                        q.Enqueue(v);//додаємо вершину в чергу, щоб її також опрацювати
+                    }
+                    if (v == dest) //якщо вершина є кінцевою
+                    {
+                        // Знайдено шуканий маршрут
+                        List<int> path = new List<int>();//створюємо список, щоб в нього помістити наш маршрут
+                        int curr = dest;//оголошуємо змінну для поточної вершини і задаємо їй значення кінцевої вершини
+                        while (curr != -1)//поки існує батьківська вершина
+                        {
+                            path.Add(curr + 1);//переходимо до нашої нумерації вершин з 1
+                            curr = parent[curr];//додаємо вершину до шляху
+                        }
+                        path.Reverse();//змінюємо маршрут на протилежний, бо в нас перша вершина кінцева, а остання перша
+                        return path;//повертаємо маршрут
+                    }
+                }
+            }
+            // Шуканий маршрут не знайдено
+            return null;
+        }
+        //додамо додання ребра із можливостю задання ваги (вартості) ребра
+        public void AddEdge(Vertex from, Vertex to, int weight)
+        {
+            var edge = new Edge(from, to, weight);
+            Edges.Add(edge);
+        }
+        public List<Tuple<int, int>>[] adjacency_list_weights()
+        {
+            List<Tuple<int, int>>[] adj = new List<Tuple<int, int>>[VertexCount];//сюди помістимо результат
+            for (int i = 0; i < VertexCount; i++) adj[i] = new List<Tuple<int, int>>();//створюємо всі елементи списку
+            foreach (Edge edge in Edges)
+                adj[edge.From.Number - 1].Add(new Tuple<int, int>(edge.To.Number - 1, edge.Weight));//додаємо в список зв'язок між вершинами
+                                                                                                    // -1 бо у нас вершини починаються з 1-ці а елементи списку з 0-ля
+            return adj;//повертаємо список суміжності
+        }
+        //визначення вершини з мінімальною вартістю, наступної, вершини до якої найдешевше переміститися
+        private int MinDistance(int[] distances, bool[] visited)//перший параметр масив відстаней, другий - масив відвіданих вершин
+        {
+            int minDistance = int.MaxValue;//задаємо мінімальне значення досить великим числом
+            int minIndex = -1; //вершина -1 (такої вершини у графі не існує)
+            for (int i = 0; i < VertexCount; i++)//перебираємо всі вершини
+            {
+                if (!visited[i]) //якщо в цій вершині ми не були
+                    if (distances[i] <= minDistance)//якщо відстань менше мінімальної
+                    {
+                        minDistance = distances[i];//зберігаємо мінімальну вартість
+                        minIndex = i;//зберігаємо індекс вершини
+                    }
+            }
+            return minIndex;//повертаємо вершину до якої саме дешевше переміститися
+        }
+        public int[] prev; //тут зберігаємо попередні (батьківські вершини), щоб потім визначити по них шлях
+        public int[] Dijkstra(int source)//розраховуємо наіменші шляхи по вартості до вершин із заданої алгоритмом Дейкстри
+        {
+            source = source - 1;//поправка, бо в нас вершини з 1, а у масиві з 0
+            int[] distances = new int[VertexCount]; //створюємо масив ля збереження вартостей переміщення
+            bool[] visited = new bool[VertexCount]; //створюємо масив відвіданих вершин графа
+            List<Tuple<int, int>>[] adjacencyList = adjacency_list_weights(); //формуємо список суміжності описаний вище
+            prev = new int[VertexCount];//в цьму масиві зберігаємо найдешевший маршрут
+                                        // ініціалізація відстаней
+            for (int i = 0; i < VertexCount; i++)//перебераємо відстані до кожної вершини
+            {
+                distances[i] = int.MaxValue;//заповнюємо відстані досить великим числом, щоб люба знайдена вартість переміщення була менше цого числа
+            }
+            distances[source] = 0;//задаємо вартість 0 для заданої вершини (вартість перемішення самої до себе)
+                                  // знаходимо найдешевший шлях до всіх вершин
+            for (int i = 0; i < VertexCount - 1; i++)//перебираємо всі вершини
+            {
+                int u = MinDistance(distances, visited);//вираховуємо мінімальну вартість до наступної вершини
+                                                        //(на першому кроці буде початкова вершина бо вартість переміщення від неїх до неї 0)
+                                                        //ця функція описана нижче
+                visited[u] = true;//помічаємо її відвіданою
+                foreach (Tuple<int, int> adjacency in adjacencyList[u])//перебираємо всі суміжні вершини з вершиною u
+                {
+                    int v = adjacency.Item1;//v - суміжна вершина
+                    int weight = adjacency.Item2;//weight - вартість переміщення по ребру
+                    if (!visited[v]) //ми не були у цій вершині
+                        if (distances[u] != int.MaxValue) //якщо вартість до вершини у масиві відстаней вирахована
+                            if (distances[u] + weight < distances[v]) //якщо знайдений шлях оптимальніший ніж вже існуючий
+                            {
+                                distances[v] = distances[u] + weight;//замінюємо значення знайденим
+                                prev[v] = u;//зберігаємо посилання на попередню вершину, щоб визначити маршрут
+                            }
+                }
+            }
+            return distances;//повертаємо масив вартостей обходу вершин
+        }
+        //функція повертає найдешевший маршрут
+        public List<int> GetPath_Dijkstra(int source, int target, int[] prev)
+        //source - вершина до якої треба дійти, target - вершина з якої починаємо рух
+        {
+            List<int> path = new List<int>();//сюди збережемо маршрут
+            int current = source;//поточна вершина = кінцевій вершині
+            while (current != target)//поки поточна вершина не рівна кінцевій
+            {
+                path.Add(current);//додаємо поточну вершину до маршруту
+                current = prev[current];//міняємо поточну вершину на батьківську
+            }
+            path.Add(target);//додаємо до шляху кінцеву вершину
+            path.Reverse();//змінюємо порядок шляху, він в нас обернений
+            return path;//повертаємо шлях
+        }
+    }
+}
---
+++ Алгоритми на графах/Lab3/Lab3/Program.cs
@@ -0,0 +1,73 @@
+﻿using Lab3;
+using System.Text;
+
+namespace Lab3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            //оголошуємо граф
+            Graph graph = new Graph();
+            //створюємо вершини 1-8
+            Vertex v1 = new Vertex(1);
+            Vertex v2 = new Vertex(2);
+            Vertex v3 = new Vertex(3);
+            Vertex v4 = new Vertex(4);
+            Vertex v5 = new Vertex(5);
+            Vertex v6 = new Vertex(6);
+            Vertex v7 = new Vertex(7);
+            Vertex v8 = new Vertex(8);
+            //додаємо вершини в граф
+            graph.AddVertex(v1);
+            graph.AddVertex(v2);
+            graph.AddVertex(v3);
+            graph.AddVertex(v4);
+            graph.AddVertex(v5);
+            graph.AddVertex(v6);
+            graph.AddVertex(v7);
+            graph.AddVertex(v8);
+            //додаемо ребра в граф, граф не орієнтований тому треба додати вершини попарно,
+            //наприклад якщо зв'язок 1-3 з вагою 5, то ми маємо ї додати зв'язок 3-1 з вагою 5
+            graph.AddEdge(v1, v2, 4);
+            graph.AddEdge(v1, v4, 3);
+            graph.AddEdge(v1, v3, 1);
+            graph.AddEdge(v2, v1, 4);
+            graph.AddEdge(v2, v3, 2);
+            graph.AddEdge(v2, v5, 4);
+            graph.AddEdge(v3, v1, 1);
+            graph.AddEdge(v3, v2, 2);
+            graph.AddEdge(v3, v6, 2);
+            graph.AddEdge(v4, v1, 3);
+            graph.AddEdge(v4, v7, 1);
+            graph.AddEdge(v5, v2, 4);
+            graph.AddEdge(v5, v6, 3);
+            graph.AddEdge(v5, v7, 5);
+            graph.AddEdge(v6, v3, 2);
+            graph.AddEdge(v6, v5, 3);
+            graph.AddEdge(v6, v8, 2);
+            graph.AddEdge(v7, v4, 1);
+            graph.AddEdge(v7, v5, 5);
+            graph.AddEdge(v7, v8, 3);
+            graph.AddEdge(v8, v6, 2);
+            graph.AddEdge(v8, v7, 3);
+            //вираховуємо найдешевші (найкоротші) шляхи обходу графу із заданої вершини
+            int[] distances = graph.Dijkstra(6);
+            //виведемо найкоротші (найдешевші) маршрути із вершини 6
+            Console.WriteLine("Вершина\tВартість Маршрут");
+            for (int i = 0; i < graph.VertexCount; i++)//перебираємо всі вершини
+            {
+                Console.Write((i + 1) + "\t" + distances[i] + " \t");//+1 бо в нас вершини починаються з 1ці, а масив з 0ля
+                List<int> path = graph.GetPath_Dijkstra(i, 5, graph.prev);//визначаємо маршрут
+                foreach (int v in path)//перебираємо всі вершини шляху і змінну v
+                {
+                    Console.Write(" " + (v + 1));//виводимо влідуючу вершину маршруту
+                }
+                Console.WriteLine();
+            }
+            //чекаємо поки користувач натисне любу клавішу на клавіатурі
+            Console.ReadKey();
+        }
+    }
+}
